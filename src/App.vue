94fade1ca3d1c4759728bd3eee7bd88ac54bfa47
kosya54 <kosya54@gmail.com>
2022-09-23 15:23:43 +0700

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+        <div v-for="employee in employees" v-bind:key="employee.id">
+          {{ employee.surname }} {{ employee.name }} {{ employee.patronymic }}
+        </div>
+      </aside>
+      <div class="content">
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      employees: null,
+      endpoint: 'http://localhost:8080/equipment/user/all'
+    }
+  },
+  created() {
+    this.getAllEmployees();
+  },
+  methods: {
+    getAllEmployees() {
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST,GET,OPTIONS, PUT, DELETE",
+          "Access-Control-Allow-Headers": "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+        },
+        auth: {
+          username: 'user',
+          password: '123'
+        }
+      }
+
+      axios.get(this.endpoint, config)
+          .then(response => {
+            this.employees = response.data;
+          })
+          .catch(error => {
+            console.log("-------error-------");
+            console.log(error);
+          })
+    }
   }
 }
 </script>
